6bbe7f7a7aaca5503bc4cfa86e2eef38e43eff23
jihyunniiii <103172971+jihyunniiii@users.noreply.github.com>
2025-05-04 23:19:53 +0900

    [6주차] 프로그래머스_Lv3_정수삼각형_배지현

---
+++ week6/프로그래머스_Lv3_정수삼각형.py
@@ -0,0 +1,15 @@
+def solution(triangle):
+    dp = [[0] * (n + 1) for n in range(len(triangle))]
+    
+    dp[0][0] = triangle[0][0]
+    
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                dp[i][j] = dp[i - 1][j] + triangle[i][j]
+            elif j == len(triangle[i]) - 1:
+                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j] + triangle[i][j], dp[i - 1][j - 1] + triangle[i][j])
+            
+    return max(dp[len(triangle) - 1])
