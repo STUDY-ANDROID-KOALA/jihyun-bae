1be2f8cca2aa9f77212e71fe3b528c05d7b795f3
jihyunniiii <103172971+jihyunniiii@users.noreply.github.com>
2025-06-19 10:13:18 +0900

    [8주차] 프로그래머스_Lv3_숫자게임_배지현

---
+++ week8/프로그래머스_Lv3_숫자게임.py
@@ -0,0 +1,11 @@
+def solution(A, B):
+    answer = 0
+    
+    A.sort()
+    B.sort()
+    
+    for i in range(len(B)):
+        if A[answer] < B[i]:
+            answer += 1
+    
+    return answer
